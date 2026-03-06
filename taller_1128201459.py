f087731d6c0816ac30b2603b6fd40d2ec36cf613
laura celedon <laura.celedon01@usa.edu.co>
2026-03-06 07:37:42 -0500

    tablas de multiplicar

--- taller_1128201459.py
+++ taller_1128201459.py
@@ -14,3 +14,22 @@ contador = 1
 while contador <= 5:
     print("numero:"+str(contador))
     contador = contador + 1
+    
+for i in range(5):
+    print("numero:"+str(i))
+
+for i in range(0, 12, 3):
+    print("numero:"+str(i))
+    
+encontrado = False
+numerobuscado = 7
+numeros = [1, 3, 5, 7, 9]
+for numero in numeros:
+    if numero == numerobuscado:
+        encontrado = True
+        break
+print   ("Número", numerobuscado, "encontrado:", encontrado)
+
+for i in range(3):
+    for j in range(1,4):
+        print("i="+str(i)+" j="+str(j))
